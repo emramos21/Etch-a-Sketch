341d42093f0027057fc9692fc1ff8b3ffe72e97b
Eva Ramos <ecolemanm13@gmail.com>
2024-03-20 11:25:18 -0500

    3/20/24

--- grid.js
+++ grid.js
@@ -1,3 +1,7 @@
-function makeGrid(){
-    
+const gridContainer = document.getElementById("gridContainer");
+for (let i = 1; i < 15; i++) {
+    const gridItem = document.createElement("div");
+    gridItem.classList.add("grid-item");
+    gridItem.textContent = i;
+    gridContainer.appendChild(gridItem);
 }
